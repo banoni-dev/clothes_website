dd529289325debcb347e80ef06d1ee47abc76930
Ahmed Barhoumi <banoni.bro@gmail.com>
2023-02-10 21:30:23 +0100

    adding sign out fonctionality and using the onAuthStateChanged firebase method

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,23 +1,38 @@
-import React from 'react'
+
+import { Fragment, useContext } from 'react';
 import './header.css'
 import logo from '../../images/logo.png'
 import { Link } from 'react-router-dom'
+import { UserContext } from '../../contexts/user.context'
+import { signOutUser } from '../../utils/firebase/firebase.utils'
 
-export default function header() {
+export default function Header() {
+  const { currentUser } = useContext(UserContext)
+  
   return (
-    <div className="header">
+    <Fragment>
+      <div className="header">
       <div className="logo">
         <Link to="/">
           <img src={logo} alt={logo} />
         </Link>
       </div>
       <div className="links">
-            <Link to="shop">Shop</Link>
-            <Link to="cathegories">Cathegories</Link>
-            <Link to='contact'>Contact</Link>
-            <Link to="sign-in">Sign in</Link>
-            <Link to="cart">Cart</Link>
+        <Link to="shop">Shop</Link>
+        <Link to="cathegories">Cathegories</Link>
+        <Link to="contact">Contact</Link>
+        {currentUser ? (
+          <a style={{cursor: 'pointer'}} onClick={signOutUser}>
+            SIGN OUT
+          </a>
+        ) : (
+          <Link to="/sign-in">
+            SIGN IN
+          </Link>
+        )}
+        <Link to="cart">Cart</Link>
       </div>
     </div>
+    </Fragment>
   )
 }
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,12 @@
-import { async } from '@firebase/util';
+
 import {initializeApp} from 'firebase/app'
-import {getAuth,signInWithPopup,GoogleAuthProvider,createUserWithEmailAndPassword,signInWithEmailAndPassword} from 'firebase/auth'
+import {getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,} from 'firebase/auth'
 import {getFirestore,doc,getDoc,setDoc} from 'firebase/firestore'
 const firebaseConfig = {
     apiKey: "AIzaSyDhV0-eRb_37YTMMP9qGmORobL5FqRLXDk",
@@ -60,3 +66,8 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 
   return await signInWithEmailAndPassword(auth, email, password);
 };
+
+export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener = (callback) =>
+  onAuthStateChanged(auth, callback);
